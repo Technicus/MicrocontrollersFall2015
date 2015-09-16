9534283c27275cb4f847c42c212abb0c275a2c0e
Technicus <fgardner@madisoncollege.edu>
2015-09-16 20:42:49 +0000

    initial commit

---
+++ learning.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   learning.c
+ * Author: technician
+ *
+ * Created on September 16, 2015, 8:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
